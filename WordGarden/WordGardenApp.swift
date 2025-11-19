8eb2b656d766cb3680e55df7751ff958044d52bc
Paul Wagstaff <paul@pwfotos.ca>
2025-11-19 15:42:07 -0700

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Paul Wagstaff on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
